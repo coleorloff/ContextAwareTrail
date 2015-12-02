02cdac30f740f6fd459827c778e84c13db73fbea
C013 <co790@nyu.edu>
2015-12-01 22:39:33 -0500

    addedupdateTags() for adding new tags to search for in db.

--- simple-trail/scripts/content_script.js
+++ simple-trail/scripts/content_script.js
@@ -5,6 +5,7 @@ var currentTrail;
 var open = false;
 var first = true;
 var tagString;
+var classTagStream = $('#tag-stream');
 
 // Talking to POPUP.js // This is popping open and closing the Trailz div in your window from the extension icon
 chrome.runtime.onMessage.addListener(
@@ -35,12 +36,15 @@ chrome.runtime.onMessage.addListener(
 $(document).ready(function(){
     toggleTrail();
     plusOne();
+    minusOne();
 });
+
 
 function loadEx(){
     var host = window.location.hostname;
     var pageBody = $(document.body);
     var pageURL = window.location.href;   
+   
     
     console.log("pageURL =" + pageURL) 
     console.log('hostname -->' + host)
@@ -48,9 +52,10 @@ function loadEx(){
 
     $('<div class="container" id="trailex" >'+'</div>').prependTo(pageBody);
     $('<div class="holder" id="trail-holder">'+'</div>').appendTo('#trailex');
+
         var htmlToAdd = 
-            '<div class="menu" id="main">'+
-                    '<h1> Az </h1>'+
+            '<div class="trailMenu" id="dash">'+
+                    //'<h1> Az </h1>'+
                         '<div class="btn-group btn-group-xs">'+
                             '<button type="button" class="btn btn-default" role="group" id="display-trail">Display Trail</button>'+
                             //creates new "trail" in db, i.e. saves current page with relevent information
@@ -60,12 +65,15 @@ function loadEx(){
                             //'<button type="button" class="btn btn-default" role="group" id="add-step">Add Step</button>'+
                         '</div>'+
             '</div>'+
-                '<div id="tagTicker">'+
+                '<div class="tagTicker" id="tagTicker">'+
                 //pls insert tags returned from Alchemy here
-                    '<div class="panel panel-default" id="tag-stream">'+
-                        '<span class="label label-six" id="add-tag">+</span>'+
+                    '<div class="panel panel-default" id="tag-stream">'+'</div>'+
+                    '<div class="input-group input-group-sm id="add-tag-div">'+
+                            '<input type="text" class="form-control" id="add-tag-input" placeholder="Enter a tag">'+
+                            '<span class="input-group-addon" id="add-tag-button">'+ 'Add' + '</span>'+
                     '</div>'+
-            '</div>';
+                '</div>'+
+                '<div style="margin-bottom:10px"></div>';
 
     jQuery('#trailex').prepend(htmlToAdd);
 }
@@ -111,10 +119,10 @@ function renderTrail(trails){
                     '<div class="panel-heading"><a href="'+trails[i].steps[j].url+'">'+trails[i].steps[j].title+'</a></div>'+
                         '<div class="panel-body>'+
                             '<ul class="list-group">'+
-                                '<li class="list-group-item" id="text"> Notes: '+trails[i].steps[j].text+'</span></li>'+
+                                '<li class="list-group-item" id="tags">'+colorTags(trails[i].steps[j].tags)+'</span></li>'+
                                 //needs some function that takes an array of tags
                                 //and puts each tag into a random label type
-                                '<li class="list-group-item" id="text"> Tags: '+colorTags(trails[i].steps[j].tags)+'</span></li>'+
+                                '<li class="list-group-item" id="text">'+trails[i].steps[j].text+'</span></li>'+
                                 //'<li class="list-group-item" id="hide url"> URL:'+trails[i].steps[j].url+'</span></li>'+
                                 //'<li class="hide list-group-item" id="hide id"> ID: '+trails[i].steps[j]._id+'</li>'+
                             '</div>'
@@ -132,9 +140,7 @@ function renderTrail(trails){
 }
 
 //on Browser Action button click --> opens the extension in the window
-function toggleContainer(){
-   $('#trailex').toggleClass('toggled');
-}
+function toggleContainer(){$('#trailex').toggleClass('toggled');}
 //on "display trail" button click --> toggles the trail open/closed
 function toggleTrail(){
     $('body').on('click', '#display-trail', function(e){
@@ -143,10 +149,23 @@ function toggleTrail(){
 };
 //adds a new label to the list of tags
 function plusOne(){
-    $('body').on('click', '#add-tag', function(e){
-        $('#tag-stream').append('<span class="label label-ten">NEW!</span>')
+    $('body').on('click', '#add-tag-button', function(e){
+        var newTag = document.getElementById('add-tag-input').value.split(',');
+        //console.log(newTag);
+        document.getElementById('add-tag-input').value = "Enter a tag";
+        for (var i = 0; i < newTag.length; i++){
+            $('#tag-stream').append('<span class="label label-ten">' + newTag[i] + '</span>');
+        }
+        updateTags(newTag);
     });
-}
+};
+
+function minusOne(){
+    $('body').on('click', '.label', function(e){
+        console.log(e.target.innerHTML);
+
+    });
+};
 
 $('body').on('click', '#add-trail', function(e){
     console.log('submitting once');
@@ -268,10 +287,8 @@ function getTags(url){
     };
 
     port.postMessage({"search": "find tags", "data": data});
-
     port.onMessage.addListener(function(msg) {
         if (msg.search == "alchemy tags returned"){
-            // console.log (msg.search);
             console.log ("tags returned to content script from alchemy -->" + msg.tags)
             tagsToString(msg.tags);
             //this function will display the tags that have been returned from Alchemy
@@ -284,6 +301,28 @@ function getTags(url){
             console.log("awesome we got tags!" + msg.taggedsteps);
             console.log(msg.taggedsteps);
 
+            //this is where the magic happens
+            //trails with matching tags get displayed
+            renderTrail(msg.taggedsteps);
+        }
+    });
+}
+
+//after you add new tags, this passes those new tags to the original
+//tag array and updates the shit you see
+function updateTags(newTags){
+    console.log("updating tags with:")
+    console.log(newTags);
+
+    var data = {  
+        tags : newTags
+    };
+
+    port.postMessage({"search": "add tags", "data": data});
+    port.onMessage.addListener(function(msg) {
+        if (msg.search == "new steps returned"){
+            console.log("awesome we got NEW tags!" + msg.taggedsteps);
+            console.log(msg.taggedsteps);
             //this is where the magic happens
             //trails with matching tags get displayed
             renderTrail(msg.taggedsteps);
@@ -336,9 +375,9 @@ function displayTags(tags){
     var colors = ['label-one', 'label-two', 'label-three', 'label-four', 'label-five', 'label-six', 'label-seven', 'label-eight', 'label-nine', 'label-ten'];
     var wrappedTags = [];
     for (var i = 0; i < tags.length; i ++){
-       wrappedTags.push('<span class ="label ' + randomLabelClass() + '">' + tags[i]+'</span>');
+       wrappedTags.push('<span class ="label ' + randomLabelClass() + ' id="'+ tags[i] + '">' + tags[i]+'</span>');
     }
-    $('#tag-stream').append(wrappedTags);
+    $('#tag-stream').append(wrappedTags.slice(0, 5));
 };
 
 
